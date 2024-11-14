bac4d0c018f19d8e44bd75f312f53c247b29231f
sch990428 <990428sch@naver.com>
2024-11-14 16:40:18 +0900

    Poolable.json을 읽어 해당하는 오브젝트 풀링
    Poolable.json을 읽어 해당하는 오브젝트 풀링

--- Move Over, Critters/Assets/Scripts/Managers/PoolManager.cs
+++ Move Over, Critters/Assets/Scripts/Managers/PoolManager.cs
@@ -1,9 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PoolManager : Singleton<PoolManager>
 {
+	public enum PoolableType
+	{
+		Player,
+		Projectile,
+	}
+
+	Dictionary<int, Data.Poolable> poolingDict;
+
 	protected override void Awake()
 	{
 		base.Awake();
+
+		poolingDict = new Dictionary<int, Data.Poolable>();
+		Init();
+	}
+
+	private void Init()
+	{
+		poolingDict = DataManager.Instance.LoadJsonToDict<Data.Poolable>("Data/Poolable");
+		
+		foreach (var poolPair in poolingDict)
+		{
+			for (int i = 0; i < poolPair.Value.PoolingAmount; i++)
+			{
+				GameObject go = ResourceManager.Instance.Instantiate(poolPair.Value.ResourcePath);
+				go.transform.SetParent(transform);
+				go.name = ((PoolableType)poolPair.Key).ToString();
+				go.SetActive(false);
+				poolPair.Value.PoolingQueue.Enqueue(go);
+			}
+		}
 	}
 }
