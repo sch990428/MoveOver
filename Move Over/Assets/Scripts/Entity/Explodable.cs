61f8142982c39fb8c3d7b1664b0efb5a23d18e80
sch990428 <990428sch@naver.com>
2024-12-02 22:30:00 +0900

    파괴된 블록 A* 알고리즘에 적용
    파괴된 블록 A* 알고리즘에 적용

--- Move Over/Assets/Scripts/Entity/Explodable.cs
+++ Move Over/Assets/Scripts/Entity/Explodable.cs
@@ -5,6 +5,12 @@ public class Explodable : MonoBehaviour
     public void Explode()
     {
         SoundManager.Instance.PlaySound(SoundManager.GameSound.Break);
+
+        // 부모의 GridMap 컴포넌트에서 자신이 있던 그리드의 
+        Vector2Int posKey = new Vector2Int((int)transform.position.x, (int)transform.position.z);
+		transform.parent.GetComponent<GridMap>().Grids[posKey].IsWalkable = true;
+        Debug.Log($"{posKey}열림");
+
         Destroy(gameObject);
     }
 }
